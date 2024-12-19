eaa38362a6602614c355903c6f23c70799063a15
itsmeani06 <161615458+itsmeani06@users.noreply.github.com>
2024-12-19 07:26:14 +0530

    Basic Setup

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
 const path=require("path");
 
+const methodOverride=require("method-override");
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 const Listing= require("./models/listing.js");
 main().then(()=>{
@@ -43,4 +46,46 @@ app.get("/listings",async (req,res)=>{
 });
 app.listen(8080,()=>{
     console.log("App is listening to port 8080");
+})
+//new route
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+//show route
+app.get("/listings/:id", async(req,res)=>{
+    let {id}= req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+})
+
+//Create route
+app.post("/listings", async(req,res)=>{
+    
+    const newListing= new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+}) 
+
+//edit Route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}= req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+})
+
+
+//Update route
+app.put("/listings/:id", async(req,res)=>{
+    let {id}= req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//delete route
+app.delete("/listings/:id", async(req, res)=>{
+    let {id}= req.params;
+    let deletedListing=await Listing.findById(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 })
